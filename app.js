5374bcfe8f7c7ab7998535f09a8ad15e971224de
Srikanth <srikanth@vestedfinance.co>
2023-05-04 23:08:29 +0530

    Added routes to generate and redirection

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const { db } = require("./src/models/db");
+const linksRoute = require("./src/routes/links");
+const redirRoute = require("./src/routes/redirection");
+
+const app = express();
+const port = 4545;
+
+app.use(express.json());
+
+app.use("/api/links", linksRoute);
+app.use("/", redirRoute);
+
+db.sync({ force: true }) // never force:true in prod, it drops dbs
+  .then(() => console.log("db works"))
+  .catch((err) => console.error(err));
+
+app.listen(port, () => {
+  console.log(`Url shortner listening on port ${port}`);
+});
